7e87f658d726ee716029cad8618ebcf83df4fb5d
kot <bafomet2016vitt@gmail.com>
2023-10-27 08:15:16 +0300

    add tests user

--- users/tests.py
+++ users/tests.py
@@ -7,7 +7,22 @@ class UserAPITest(APITestCase):
     def setUp(self) -> None:
         self.client = APIClient()
 
-    def test_user_create(self):
-        data = {'email': 'user@example.com', 'password': 'Qf1fgj_45ty14$'}
+    def test_user_create_and_jwt(self):
+        data = {
+            'email': 'user@example.com',
+            'password': 'Qf1fgj_45ty14$',
+            'first_name': 'Exam',
+            'last_name': 'Ple',
+            'patronymic': 'Lost',
+            'phone': '+7(999)567-57-00',
+            'city': 'Vrn'
+        }
         response = self.client.post('/user/create-user/', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data = {
+            'email': 'user@example.com',
+            'password': 'Qf1fgj_45ty14$'
+        }
+        response = self.client.post('/user/get-token/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
