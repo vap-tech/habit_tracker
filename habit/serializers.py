31e91eb9deab4770eaec4d6edcb5b1981e2fb9a3
kot <bafomet2016vitt@gmail.com>
2023-11-09 00:50:22 +0300

    add serializer

---
+++ habit/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from habit.models import Habit
+from habit.validators import HabitValidator
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    """Habit serializer"""
+    class Meta:
+        model = Habit
+        fields = (
+            'name',
+            'owner',
+            'place',
+            'time',
+            'action',
+            'is_nice',
+            'related',
+            'period',
+            'reward',
+            'time_to_complete',
+            'is_public'
+        )
+        validators = [HabitValidator()]
