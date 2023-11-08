0853a008d1a1590dc1496c512c8b0d4f6caf0bf8
kot <bafomet2016vitt@gmail.com>
2023-11-08 06:02:03 +0300

    fix model habit

--- habit/models.py
+++ habit/models.py
@@ -10,11 +10,11 @@ class Habit(models.Model):
     place = models.CharField(max_length=200, verbose_name='место', **NULLABLE)
     time = models.TimeField(verbose_name='время', **NULLABLE)
     action = models.CharField(verbose_name='действие', **NULLABLE)
-    is_nice = models.BooleanField(verbose_name='это приятно')
+    is_nice = models.BooleanField(verbose_name='это приятно', **NULLABLE)
     related = models.ForeignKey('self', verbose_name='связанная', on_delete=models.SET_NULL, **NULLABLE)
     period = models.PositiveIntegerField(verbose_name='периодичность', default=1)
     reward = models.CharField(verbose_name='награда', max_length=250, **NULLABLE)
-    time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение', default=2, **NULLABLE)
+    time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение', default=45, **NULLABLE)
     is_public = models.BooleanField(verbose_name='опубликовано', default=False, **NULLABLE)
 
     def __str__(self):
